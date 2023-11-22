64f75173a0dec50f585bfd03557ac2b9cef75aa5
ssdn7 <somsodana9@gmail.com>
2023-11-23 00:01:10 +0700

    home new

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,20 @@
 import React from "react";
 
-<<<<<<< HEAD
-import Footer from '../components/Footer'
-import Footer2 from '../components/Footer2'
-import Post from '../components/Form/Post'
-import Hero from '../components/Home/Hero'
-import Benefit from '../components/home/Benefits'
-import Faq from '../components/home/Faq'
-import Quote from '../components/home/Quote'
-import Who from '../components/home/Whoarewe'
-import WhyOrg from '../components/home/WhyOrganization'
-import Why from '../components/home/WhySection'
-=======
-import Footer from "../components/Footer";
-import Hero from "../components/Home/Hero";
-import Benefit from "../components/home/Benefits";
-import Faq from "../components/home/Faq";
-import Who from "../components/home/Whoarewe";
-import WhyOrg from "../components/home/WhyOrganization";
-import Why from "../components/home/WhySection";
->>>>>>> 66517d617ed8ad3f26ca051897f6645c7dfb6e1e
+import Footer from '../components/Footer';
+import Footer2 from '../components/Footer2';
+import Post from '../components/Form/Post';
+import Hero from '../components/Home/Hero';
+import Benefit from '../components/home/Benefits';
+import Faq from '../components/home/Faq';
+import Quote from '../components/home/Quote';
+import Who from '../components/home/Whoarewe';
+import WhyOrg from '../components/home/WhyOrganization';
+import Why from '../components/home/WhySection';
+
 
 const Home = () => {
   return (
     <>
-<<<<<<< HEAD
         {/* <Header /> */}
         <Hero />
         <Who />
@@ -36,16 +26,15 @@ const Home = () => {
         <Footer />
         <Post/>
      <Footer2/>
-=======
-      {/* <Header /> */}
+{/* =======
+      
       <Hero />
       <Who />
       <Why />
       <WhyOrg />
       <Benefit />
       <Faq />
-      <Footer />
->>>>>>> 66517d617ed8ad3f26ca051897f6645c7dfb6e1e
+      <Footer /> 66517d617ed8ad3f26ca051897f6645c7dfb6e1e */}
     </>
   );
 };

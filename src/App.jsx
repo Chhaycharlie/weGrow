66517d617ed8ad3f26ca051897f6645c7dfb6e1e
sunchengchhay <sonchhay2018@gmail.com>
2023-11-22 22:03:32 +0700

    modal update

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 // import { Header } from './components/Header'
-import Community from './pages/Community'
-import Home from './pages/Home'
-import Recruitment from './pages/Recruitment'
-import Signup from './pages/Signup'
+import Community from "./pages/Community";
+import Home from "./pages/Home";
+import Recruitment from "./pages/Recruitment";
+import Signup from "./pages/Signup";
 //  import Footer from './components/Footer'
-import Header from './components/Header'
-import Contact from './pages/Contact'
-import Login from './pages/Login'
-
+import Header from "./components/Header";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
 
 function App() {
- return (
-   <>
-    <BrowserRouter>
-    <Header/>
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
         <Routes>
-                <Route path="/" index element={<Home />} />
-                <Route path="/Recruitment" element={<Recruitment />} />
-                <Route path="/Community" element={<Community/>}/>
-                <Route path="/Login" element={<Login />} />
-                <Route path="/Signup" element={<Signup />} />
-                <Route path="/Contact" element={<Contact />} />
+          <Route path="/" index element={<Home />} />
+          <Route path="/Recruitment" element={<Recruitment />} />
+          <Route path="/Community" element={<Community />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/Contact" element={<Contact />} />
         </Routes>
-    </BrowserRouter>
-         </>
-   )
- }
+      </BrowserRouter>
+    </>
+  );
+}
 
-export default App
-
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
+import React from "react";
 
-import Footer from '../components/Footer'
-import Hero from '../components/Home/Hero'
-import Benefit from '../components/home/Benefits'
-import Faq from '../components/home/Faq'
-import Who from '../components/home/Whoarewe'
-import WhyOrg from '../components/home/WhyOrganization'
-import Why from '../components/home/WhySection'
+import Footer from "../components/Footer";
+import Hero from "../components/Home/Hero";
+import Benefit from "../components/home/Benefits";
+import Faq from "../components/home/Faq";
+import Who from "../components/home/Whoarewe";
+import WhyOrg from "../components/home/WhyOrganization";
+import Why from "../components/home/WhySection";
 
 const Home = () => {
   return (
     <>
-        {/* <Header /> */}
-        <Hero />
-        <Who />
-        <Why />
-        <WhyOrg />
-        <Benefit />
-        <Faq />
-        <Footer />
+      {/* <Header /> */}
+      <Hero />
+      <Who />
+      <Why />
+      <WhyOrg />
+      <Benefit />
+      <Faq />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

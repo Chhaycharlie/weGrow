b364fef1110c1aaae383e038e885ee11036e3e87
sunchengchhay <sonchhay2018@gmail.com>
2023-11-23 23:53:35 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 // import { Header } from './components/Header'
-<<<<<<< HEAD
 import Course from './pages/Course'
 import Home from './pages/Home'
 import Recruitment from './pages/Recruitment'
@@ -13,16 +12,6 @@ import Content from './components/inspiration/Content'
 import Contact from './pages/Contact'
 import Login from './pages/Login'
 
-=======
-import Community from "./pages/Community";
-import Home from "./pages/Home";
-import Recruitment from "./pages/Recruitment";
-import Signup from "./pages/Signup";
-//  import Footer from './components/Footer'
-import Header from "./components/Header";
-import Contact from "./pages/Contact";
-import Login from "./pages/Login";
->>>>>>> 66517d617ed8ad3f26ca051897f6645c7dfb6e1e
 
 function App() {
   return (
@@ -30,7 +19,6 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-<<<<<<< HEAD
                 <Route path="/" index element={<Home />} />
                 <Route path="/Recruitment" element={<Recruitment />} />
                 <Route path="/Course" element={<Course/>}/>
@@ -39,14 +27,6 @@ function App() {
                 <Route path="/Contact" element={<Contact />} />
                 <Route path="/VolunteerForm" element={<VolunteerForm/>} />
                 <Route path="/Content" element={<Content/>} />
-=======
-          <Route path="/" index element={<Home />} />
-          <Route path="/Recruitment" element={<Recruitment />} />
-          <Route path="/Community" element={<Community />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/Signup" element={<Signup />} />
-          <Route path="/Contact" element={<Contact />} />
->>>>>>> 66517d617ed8ad3f26ca051897f6645c7dfb6e1e
         </Routes>
       </BrowserRouter>
     </>

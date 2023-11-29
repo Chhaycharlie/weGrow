91f9d8890986fd6f9149451f23bd5920558e85c7
ssdn7 <somsodana9@gmail.com>
2023-11-29 22:30:41 +0700

    update 500 server

---
+++ src/pages/ErrorServer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function ErrorServer() {
+  return (
+    <>
+    <div className="w-full h-screen flex flex-col items-center justify-center">
+    <img src="/src/assets/image/500.jpg" alt="image" className="w-1/2 md:w-1/3 lg:w-1/4 text-blue-600"/>
+        <div className="flex flex-col items-center justify-center">
+            <p className="text-5xl md:text-6xl lg:text-7xl font-bold tracking-wider text-gray-600">500</p>
+            <p className="text-2xl md:text-3xl lg:text-4xl font-bold text-gray-600 mt-2">Server Error</p>
+            <p className="md:text-lg xl:text-xl text-gray-500 mt-4">Whoops, something went wrong on our servers.</p>
+        </div>
+    </div>
+</>
+  )
+}
+
+export default ErrorServer
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import Footer from "../components/shared/Footer";
-import Header from "../components/shared/Header";
-import Hero from "../components/home/Hero";
 import Benefit from "../components/home/Benefits";
 import Faq from "../components/home/Faq";
+import Hero from "../components/home/Hero";
+import Quote from "../components/home/Quote";
 import Who from "../components/home/Whoarewe";
 import WhyOrg from "../components/home/WhyOrganization";
 import Why from "../components/home/WhySection";
-import Quote from "../components/home/Quote";
+import Footer from "../components/shared/Footer";
+import Header from "../components/shared/Header";
 
 const Home = () => {
   return (
